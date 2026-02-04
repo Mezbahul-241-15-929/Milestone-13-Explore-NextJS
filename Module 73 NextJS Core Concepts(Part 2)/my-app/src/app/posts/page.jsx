a7f73fa1465b6a25c56ac0e06b3bdf5fc4b1798b
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2026-02-05 03:42:49 +0600

    73-3 Data Fetching with SSR

---
+++ Module 73 NextJS Core Concepts(Part 2)/my-app/src/app/posts/page.jsx
@@ -0,0 +1,25 @@
+export const getPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return data;
+};
+
+export default async function Posts() {
+    const posts = await getPosts();
+
+    return (
+        <div className="gap-8 grid grid-cols-4">
+            {
+                posts.map((singlePost) => {
+                    return (
+                        <div key={singlePost.id}>
+                            <p>{singlePost.id}</p>
+                            <p>{singlePost.title}</p>
+                            <p>{singlePost.body}</p>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
+}
