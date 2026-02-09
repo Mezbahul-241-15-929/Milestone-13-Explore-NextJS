47cd937ef85944a6de71a4da6fbb0066aaa1c429
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2026-02-09 19:34:08 +0600

    75-3 Get Currently Logged In user in Client and Server component

--- Module 75 Authentication in NextJs with Next Auth/my-app/src/app/page.js
+++ Module 75 Authentication in NextJs with Next Auth/my-app/src/app/page.js
@@ -1,6 +1,10 @@
+import UserInfo from "@/components/UserInfo";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session= await getServerSession(authOptions)
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -59,6 +63,9 @@ export default function Home() {
             Documentation
           </a>
         </div>
+        <p>From client component</p>
+        <UserInfo/>
+        {JSON.stringify(session)}
       </main>
     </div>
   );
