9c3c1a5499e455b1775af902d941bbec888100f4
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2026-02-14 01:31:30 +0600

    76-9 Fix redirect issue and Post SocialLogin data on MongoDB

--- Module 76 Car Doctor Pro with NextJS Part-1/my-app/src/app/api/auth/[...nextauth]/route.js
+++ Module 76 Car Doctor Pro with NextJS Part-1/my-app/src/app/api/auth/[...nextauth]/route.js
@@ -1,41 +1,7 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-import { loginUser } from "@/app/actions/auth/loginUser";
+import { authOptions } from "@/lib/authOptions";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        const user = await loginUser(credentials);
 
-        if (!user) return null;
-
-        return user;
-      }
-    }),
-
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    })
-  ],
-
-  pages: {
-    signIn: "/login"
-  }
-};
 
 const handler = NextAuth(authOptions);
 
